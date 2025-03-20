703190e7701fa6bbaa132817aaf4e079b9a7637e
A6l9 <sfsf.sfsf.18@list.ru>
2025-03-20 23:11:47 +0500

    fixing timezone and date comparison error

--- utils/check_end_date.py
+++ utils/check_end_date.py
@@ -6,11 +6,10 @@ from database.get_db_interface import db_interface
 
 
 async def check_challenge_end_date(chall_list: list) -> None:
-    date_now = datetime.now(tz=timezone("Moscow/Europe"))
+    date_now = datetime.now(tz=timezone("Europe/Moscow")).date()
     ended_challenges = []
     for i_chall in chall_list:
-        chall_date_end = datetime.strptime(i_chall.date_of_end, "%Y-%m-%d %H:%M")
-        if date_now < chall_date_end:
+        if date_now > i_chall.date_of_end:
             ended_challenges.append(i_chall.challenge_id)
     if ended_challenges:
         await db_interface.change_challenges_status(challenge_ids=ended_challenges, status=True)
