60c5f0a467f917d7a6d7daab6bc52fba9eedf68a
A6l9 <sfsf.sfsf.18@list.ru>
2025-03-20 22:53:30 +0500

    adding reply comments, cancel routers

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 
 from loader import dp, bot, logger
+from handlers.custom.comments_n_posts_check import router as new_comments_n_posts_router
+from handlers.custom.reply_to_comment import router as reply_comment_router
+from handlers.custom.cancel_handler import router as cancel_router
 from database.get_db_interface import db_interface
 
 
@@ -12,6 +15,7 @@ async def on_startup() -> None:
 async def main() -> None:
     bot_info = await bot.get_me()
     logger.info(f"The bot \"{bot_info.full_name}\" started working")
+    dp.include_routers(reply_comment_router, new_comments_n_posts_router, cancel_router)
     await dp.start_polling(bot)
 
 
