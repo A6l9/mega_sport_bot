8b46eb8bbfa2e0c1b74b4c9d37341000f9dbbd7b
A6l9 <sfsf.sfsf.18@list.ru>
2025-03-20 23:13:48 +0500

    adding additional options of keywords

--- utils/check_keywords_in_message.py
+++ utils/check_keywords_in_message.py
@@ -2,9 +2,11 @@ import re
 
 
 async def check_message(message_text: str) -> bool:
-    keywords = ["#челендж" "челенджа", "челендж", "челенджей", "челенджи"]
-    pattern = "|".join(rf"\b{i}\b" for i in keywords).replace(" ", r"\s+")
-    result = re.findall(pattern, message_text, re.IGNORECASE)
-    if result:
-        return True
-    return False
+    if message_text:
+        keywords = ["#челлендж", "челленджа", "челлендж", "челленджей", "челленджи",
+                    "#челендж" "челенджа", "челендж", "челенджей", "челенджи"]
+        pattern = "|".join(rf"\b{i}\b" for i in keywords).replace(" ", r"\s+")
+        result = re.findall(pattern, message_text, re.IGNORECASE)
+        if result:
+            return True
+        return False
