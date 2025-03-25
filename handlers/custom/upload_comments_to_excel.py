a03164ca8db6ae99665c0b4fcf913565dacb8432
A6l9 <sfsf.sfsf.18@list.ru>
2025-03-25 18:14:31 +0500

    correcting comments uploading

--- handlers/custom/upload_comments_to_excel.py
+++ handlers/custom/upload_comments_to_excel.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
 
 from database.get_db_interface import db_interface
 from database.models import Comments
@@ -11,14 +12,14 @@ from loader import bot
 router = Router(name="upload_comments")
 
 
-@router.message(F.text == "upload_comments")
+@router.message(F.text.startswith("/upload_comments"))
 @admins_group_access
-async def upload_comments(message: Message) -> None:
+async def upload_comments(message: Message, state: FSMContext) -> None:
+    await state.clear()
     comments = await db_interface.get_row(Comments, to_many=True)
     if comments:
         await upload_comments_to_excel(comments)
-        file = open(file="comments.xlsx", mode="rb")
-        await bot.send_document(document=file,
+        await bot.send_document(document=FSInputFile("comments.xlsx", filename="comments.xlsx"),
                                 chat_id=message.chat.id)
     else:
         await message.answer("Комментарии пока не были написаны.")
