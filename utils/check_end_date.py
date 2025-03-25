47f2462413be53491a555c841cb1d85ebb3fa9ea
A6l9 <sfsf.sfsf.18@list.ru>
2025-03-25 18:16:11 +0500

    adding a boolean value returning

--- utils/check_end_date.py
+++ utils/check_end_date.py
@@ -5,7 +5,7 @@ from pytz import timezone
 from database.get_db_interface import db_interface
 
 
-async def check_challenge_end_date(chall_list: list) -> None:
+async def check_challenge_end_date(chall_list: list) -> bool:
     date_now = datetime.now(tz=timezone("Europe/Moscow")).date()
     ended_challenges = []
     for i_chall in chall_list:
@@ -13,3 +13,5 @@ async def check_challenge_end_date(chall_list: list) -> None:
             ended_challenges.append(i_chall.challenge_id)
     if ended_challenges:
         await db_interface.change_challenges_status(challenge_ids=ended_challenges, status=True)
+        return False
+    return True
